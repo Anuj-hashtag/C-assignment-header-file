f50e6c1fccfe99bb4c4f2a6c23dfef0858ca0dd0
Anuj-hashtag <anujsurya1122@gmail.com>
2025-04-29 23:19:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
+// main.c
+
 #include <stdio.h>
 #include "file.h"
 
 int main() {
     int year, num, size;
+    int is_leap, is_armstrong, largest_num;
 
     printf("Enter a year: ");
     scanf("%d", &year);
-    leap_year(year);
+    leap_year(year, &is_leap);
     if (is_leap) printf("%d is a leap year.\n", year);
     else printf("%d is NOT a leap year.\n", year);
 
     printf("Enter a number: ");
     scanf("%d", &num);
-    armstrong(num);
+    armstrong(num, &is_armstrong);
     if (is_armstrong) printf("%d is an Armstrong number.\n", num);
     else printf("%d is NOT an Armstrong number.\n", num);
 
@@ -21,7 +24,7 @@ int main() {
     int arr[size];
     printf("Enter %d numbers: ", size);
     for (int i = 0; i < size; i++) scanf("%d", &arr[i]);
-    largest(arr, size);
+    largest(arr, size, &largest_num);
     printf("Largest number: %d\n", largest_num);
 
     return 0;
